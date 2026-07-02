f0e65d618c9b2792180affee46a3a8471272fef0
srcm333 <srcm333@gmail.com>
2026-07-02 23:02:33 +0530

    terraform

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "terraform_data" "main" {
   }
 }
 
-/* resource "aws_ec2_instance_state" "main" {
+resource "aws_ec2_instance_state" "main" {
   instance_id = aws_instance.main.id
   state       = "stopped"
   depends_on = [terraform_data.main]
@@ -203,4 +203,4 @@ resource "terraform_data" "main_delete" {
   provisioner "local-exec" {
     command = "aws ec2 terminate-instances --instance-ids ${aws_instance.main.id}"
   }
-} */
+}
