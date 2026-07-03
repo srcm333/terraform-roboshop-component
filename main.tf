8b76cedcf140b2e5bb2017cb882b9c943c2df731
srcm333 <srcm333@gmail.com>
2026-07-03 15:56:35 +0530

    terraform

--- main.tf
+++ main.tf
@@ -193,7 +193,7 @@ resource "aws_lb_listener_rule" "main" {
   }
 }
 
-/* resource "terraform_data" "main_delete" {
+resource "terraform_data" "main_delete" {
   triggers_replace = [
     aws_instance.main.id
   ]
@@ -203,4 +203,4 @@ resource "aws_lb_listener_rule" "main" {
   provisioner "local-exec" {
     command = "aws ec2 terminate-instances --instance-ids ${aws_instance.main.id}"
   }
-} */
+}
